3e6cf93edeb856a40b66db179e738f0f0fc98ac5
Mayuu_ <40952685+samirmedani@users.noreply.github.com>
2022-04-10 21:28:59 +0200

    ajout du système de calcule

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ var input = " ";
 
 
 function ShowValue(btn) {
+    
     input = input + btn.value;
 
-    console.log(eval(input));
+    document.getElementById("result").innerHTML = input
 
+}
+
+function quickMath() {
+    document.getElementById("result").innerHTML = eval(input)
+}
+
+function Reset() {
     
-    
+    document.getElementById("result").innerHTML = "0"
+    input = ""
 
 }
